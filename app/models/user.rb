17c76615d6c8a7bd27e1373f61c917601221d478
motooga <hashioga2017@gmail.com>
2023-06-04 19:16:08 +0900

    ユーザー管理機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,10 +7,12 @@ class User < ApplicationRecord
   validates :email, presence: true
   validates :encrypted_password, presence: true
   validates :nickname, presence: true
-  validates :family_name, presence: true
-  validates :name, presence: true
-  validates :kana_family_name, presence: true
-  validates :kana_name, presence: true
-  validates :birthday, presence: true
+  validates :last_name, presence: true
+  validates :first_name, presence: true
+  validates :last_name_kana, presence: true
+  validates :first_name_kana, presence: true
+  validates :birth_date, presence: true
 
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+validates :password, format: { with: VALID_PASSWORD_REGEX }
 end
