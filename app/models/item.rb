eab9b2ced9d257573e1d82efe3faf05b77c6cd25
motooga <hashioga2017@gmail.com>
2023-06-11 06:37:08 +0900

    ActiveHashによるプルダウン選択の追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,15 @@
 class Item < ApplicationRecord
 belongs_to :user
 
+extend ActiveHash::Associations::ActiveRecordExtensions
+belongs_to :item_category, :item_sales_status_id
+
+validates :item_name, presence: true
+validates :item_info, presence: true
+validates :item_price, presence: true
+validates :item_category_id, numericality: { other_than: 1 }
+validates :item_prefecture_id, numericality: { other_than: 1 }
+validates :item_sales_status_id, numericality: { other_than: 1 }
+validates :item_sheduled_derivery_id, numericality: { other_than: 1 }
+validates :item_shipping_fee_status_id, numericality: { other_than: 1 }
 end
