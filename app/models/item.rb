7538a752741f9c735b3436618fd88a5b9ecaeeb1
motooga <hashioga2017@gmail.com>
2023-06-11 09:54:07 +0900

    画像のバリデーション追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,7 @@
 class Item < ApplicationRecord
 belongs_to :user
 has_one_attached :image
+validates :image, presence: true
 
 extend ActiveHash::Associations::ActiveRecordExtensions
 belongs_to :item_category
