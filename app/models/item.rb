57c438febd5544ff143b194035fb6ef20b0c631c
motooga <hashioga2017@gmail.com>
2023-06-11 10:04:07 +0900

    商品価格のバリデーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -13,10 +13,13 @@ belongs_to :item_shipping_fee_status
 
 validates :item_name, presence: true
 validates :item_info, presence: true
-validates :item_price, presence: true
+validates :item_price, presence: true, numericality: { greater_than:300, less_than:9999999 }
 validates :item_category_id, numericality: { other_than: 1 }
 validates :item_prefecture_id, numericality: { other_than: 1 }
 validates :item_sales_status_id, numericality: { other_than: 1 }
 validates :item_scheduled_delivery_id, numericality: { other_than: 1 }
 validates :item_shipping_fee_status_id, numericality: { other_than: 1 }
+
+
+
 end
