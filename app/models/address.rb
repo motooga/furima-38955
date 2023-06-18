fa5425d6faeac3474683b5a36eb1227d335ead52
motooga <hashioga2017@gmail.com>
2023-06-18 21:43:46 +0900

    アソシエーションの記述とバリデーション

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,2 +1,11 @@
 class Address < ApplicationRecord
+  belongs_to :order
+
+  validates :postal_code, presence: true
+  validates :item_shipping_fee_status, presence: true
+  validates :city, presence: true
+  validates :street, presence: true
+  validates :phone_number, presence: true, numericality: { only_integer: true }
+  validates :birth_date, presence: true
+  validates :order, presence: true
 end
