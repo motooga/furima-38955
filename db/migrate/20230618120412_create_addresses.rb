0b59dfa8bd9b353d5a969e2c697f73773ddb438e
motooga <hashioga2017@gmail.com>
2023-06-18 21:15:19 +0900

    モデルの作成

---
+++ db/migrate/20230618120412_create_addresses.rb
@@ -0,0 +1,15 @@
+class CreateAddresses < ActiveRecord::Migration[6.0]
+  def change
+    create_table :addresses do |t|
+      t.string :postal_code, null: false
+      t.integer :item_shipping_fee_status_id, null: false
+      t.string :city, null: false
+      t.string :street, null: false
+      t.string :builing_name, null: false
+      t.string :phone_number, null: false
+      t.references :order, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
