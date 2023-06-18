9b2c259cf8ca1eab581f55dc5f10b5eb5136308c
motooga <hashioga2017@gmail.com>
2023-06-19 01:48:59 +0900

    addressのルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'items#index'
-  resources :items
+  resources :items do 
+    resources :orders, only: [:new, :create] do
+      resources :address, only: [:new, :create]
+    end
+  end
 end
